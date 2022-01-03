bc081ebe7e74d448088a565b7eb2a545096583ed
hecuhecu <hecunyuji5@gmail.com>
2022-01-03 15:19:44 +0900

    first commit

--- hirokiKadai6/ContentView.swift
+++ hirokiKadai6/ContentView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNum: Int = 1
+    @State private var sliderNum: Double = 1.0
+    @State private var judgeAlert: Bool = false
+    @State private var alertMessage: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(randomNum)")
+                .font(.largeTitle)
+                .padding()
+            
+            Slider(value: $sliderNum, in: 1...100)
+                .padding(.horizontal)
+
+            HStack {
+                Text("1")
+                    .padding()
+                
+                Spacer()
+                
+                Text("100")
+                    .padding()
+            }
+            
+            Button(action: {
+                numJudge()
+                judgeAlert = true
+                randomNum = Int.random(in: 1...100)
+            }) { Text("判定！") }
+            .alert("結果", isPresented: $judgeAlert, actions: {}, message: {Text(alertMessage)})
+        }
+    }
+    
+    private func numJudge() {
+        if randomNum == Int(sliderNum) {
+            alertMessage = "あたり！\n" + "あなたの値: \(Int(sliderNum))"
+        } else {
+            alertMessage = "はずれ！\n" + "あなたの値: \(Int(sliderNum))"
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
